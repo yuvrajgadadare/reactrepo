9c56871d1f5c8463be5672421951ab7e84d4d20f
yuvrajgadadare <yuvraj.gadadare@gmail.com>
2026-05-21 08:26:19 +0530

    added

---
+++ src/Sample.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const Sample=()=>{
+    const [count,setCounter]=useState(0);
+    const Increment=()=>{
+         var cnt=count;
+         cnt++;
+        setCounter(cnt);
+    }
+    return(
+        <div style={{"paddingLeft":"20%"}}>
+            <h2>Welcome to My First React App</h2>
+            <h2>{count}</h2>
+            <button onClick={Increment}>Click</button>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Sample } from './Sample';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Sample />
   </React.StrictMode>
 );
 
